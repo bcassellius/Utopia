0121e0ce6d31b5f65fbd266cff5e03347c2af127
Olga <o.smolyakova30@gmail.com>
2021-07-11 14:36:35 -0500

    added weather serch API

--- assets/js/scriptapi.js
+++ assets/js/scriptapi.js
@@ -18,14 +18,14 @@ function runLater() {
     });
 }
 
-let mapquestApi = `http://www.mapquestapi.com/search/v3/prediction?key=M6cWf6SB2TBYZpZZyd6wL6kpI31d0emQ&limit=5&collection=poi&q=milwaukee`;
-fetch(mapquestApi)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.results);
+// let mapquestApi = `http://www.mapquestapi.com/search/v3/prediction?key=M6cWf6SB2TBYZpZZyd6wL6kpI31d0emQ&limit=5&collection=poi&q=milwaukee`;
+// fetch(mapquestApi)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data.results);
 
-    for (i = 0; i < data.results.length; i++) console.log(data.results[i].name);
-  });
+//     for (i = 0; i < data.results.length; i++) console.log(data.results[i].name);
+//   });
 
 //   testing
 let key = `zyAk0vwUuaz2iWvaUGfsN9GfdQyi5dFO`;
@@ -60,6 +60,10 @@ fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
   
   
 //   // Weather api
+
+
+const apiKey = '5569f0d8093687922f5c0ba190e02e6c' // Olga's APIkey
+
   
 //   // Bri's Key:  d062b7cc2ea4bdcd13c368fce11ee8b1
   
@@ -118,3 +122,24 @@ fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
 //     })
 //   })
 // }  
+
+function updateWeatherModal() {
+  let city = $("#weather-search").val().trim()
+  if (!city) {
+    $('#weather-modal-content').text(`Please, enter city name.`)
+  } else {
+  let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  fetch(URL).then(function(response){
+    if (response.ok){
+      response.json().then(function(data){
+        console.log(data)
+        $('#weather-modal-content').text(`Current weather in ${data.name} is ${data.main.temp} and ${data.weather[0].description}.`)
+      })
+    }else{
+      $('#weather-modal-content').text(`Please, enter correct city name.`)
+    }
+  })
+  }
+};
+
+$('#city-search-button').on("click", updateWeatherModal)
