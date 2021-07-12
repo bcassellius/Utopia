d350c87d758855512af44ec897766a278e0686e0
Lashaun <clarklas831@gmail.com>
2021-07-12 13:20:53 -0500

    pull from storage in search tab

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -8,3 +8,15 @@ $(document).ready(function () {
     $(`.carousel.carousel-slider`).carousel(`next`);
   }, 5000);
 });
+
+let searchLocation = localStorage.getItem(`Search Location`);
+$(`#city-name`).text(searchLocation);
+
+// let mapquestApi = `https://www.mapquestapi.com/search/v3/prediction?key=M6cWf6SB2TBYZpZZyd6wL6kpI31d0emQ&limit=5&collection=poi&q=milwaukee`;
+// fetch(mapquestApi)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data.results);
+
+//     for (i = 0; i < data.results.length; i++) console.log(data.results[i].name);
+//   });
--- assets/js/scriptapi.js
+++ assets/js/scriptapi.js
@@ -41,15 +41,6 @@ function findSearchLocation() {
 }
 $(`#city-search-button`).on(`click`, findSearchLocation);
 
-// let mapquestApi = `http://www.mapquestapi.com/search/v3/prediction?key=M6cWf6SB2TBYZpZZyd6wL6kpI31d0emQ&limit=5&collection=poi&q=milwaukee`;
-// fetch(mapquestApi)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data.results);
-
-//     for (i = 0; i < data.results.length; i++) console.log(data.results[i].name);
-//   });
-
 // Weather api
 const apiKey = '5569f0d8093687922f5c0ba190e02e6c'; // Olga's APIkey
 
