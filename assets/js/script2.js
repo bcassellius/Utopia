831503b6a974dbab08d116273b4c4e19ff654bf7
Bri <bhilliker@gmail.com>
2021-07-12 20:38:23 -0500

    Weather display on second page

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -28,7 +28,7 @@ const apiKey = 'd062b7cc2ea4bdcd13c368fce11ee8b1' // Bri's APIkey
 
 
 function collectCityData() {
-  const forcast = $('#city-name');
+  const forcast = $('#city-name').text();
   let URL = `https://api.openweathermap.org/data/2.5/weather?q=${forcast}&units=imperial&appid=${apiKey}`;
   fetch(URL).then(function (response){
     if (response.ok){
@@ -36,31 +36,34 @@ function collectCityData() {
       .then(function (data){
         console.log(data);
         renderForecast(forcast)
+        // Create a card for today's forcast
+        
+        function renderForecast() {
+        let stamp = data.dt
+        const when = new Date(stamp * 1000)
+        const forcastdate = Intl.DateTimeFormat("en-US").format(when)
+        const look = data.weather[0].icon
+        const temp = data.main.temp
+        const wind = data.wind.speed
+        const humidity = data.main.humidity
+        const card = `<div class="col s12 weather-box"></div>
+        <h3>Weather:</h3>
+        <div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item date">${forcastdate}</li>
+        <img class="list-group-item weather-icon" src="http://openweathermap.org/img/wn/${look}@2x.png" alt="">
+        <li class="list-group-item temp">Temp: ${temp}&#8457</li>
+        <li class="list-group-item wind">Wind: ${wind} MPH</li>
+        <li class="list-group-item humidity">Humidity: ${humidity}%</li>
+        </ul>
+        </div>`    
+        console.log(card)
+        $('.forecast-container').prepend(card)
+        console.log( $('.forecast-container'))
+        }
       })
     }
   })
 };
 
-// Create a card for today's forcast
-function renderForecast(forcast) {
-let stamp = daily.data.dt
-const when = new Date(stamp * 1000)
-const forcastdate = Intl.DateTimeFormat("en-US").format(when)
-const look = data.daily.weather[0].icon
-const temp = data.daily.temp.day
-const wind = data.daily.wind_speed
-const humidity = data.daily.humidity
-const card = `<div class="col s12 weather-box"></div>
-          <h3>Weather:</h3>
-          <div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item date">${forcastdate}</li>
-              <img class="list-group-item weather-icon" src="http://openweathermap.org/img/wn/${look}@2x.png" alt="">
-              <li class="list-group-item temp">Temp: ${temp}&#8457</li>
-              <li class="list-group-item wind">Wind: ${wind} MPH</li>
-            <li class="list-group-item humidity">Humidity: ${humidity}%</li>
-            </ul>
-          </div>`    
-  $('.forecast-container').append(card)
-}
 collectCityData()
